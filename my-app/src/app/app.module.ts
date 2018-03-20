d9dacf2f12f96a153aa3f37f6f70522d54b6af01
Brady <wlt157459@163.com>
2018-03-20 11:48:16 +0800

    angular learn end

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,25 +1,35 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from "@angular/forms";
-
-
+import {HeroService} from "./hero.service";
 import {AppComponent} from './app.component';
 import {HeroesComponent} from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    HeroService,
+    MessageService,
+  ],
+  bootstrap: [AppComponent],
+
 })
 export class AppModule {
 }
